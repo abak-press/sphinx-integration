51fba5e733b641a7daa740a8fc9932a25069b4b1
Merkushin <merkushin.m.s@gmail.com>
2013-03-21 14:08:24 +0600

    ranged query fixes fixes

--- lib/sphinx_integration/extensions/attribute.rb
+++ lib/sphinx_integration/extensions/attribute.rb
@@ -17,11 +17,29 @@ module SphinxIntegration::Extensions::Attribute
   end
 
   def source_value_with_custom_query(offset, delta)
-    if @query.present?
+    if is_string?
+      return "#{query_source.to_s.dasherize}; #{columns.first.__name}"
+    end
+
+    if query_source == :ranged_query
+      if @query.present?
+        query = @query.gsub(/\{\{([a-zA-Z0-9_.]+)\}\}/, "\\1 #{ThinkingSphinx.unique_id_expression(adapter, offset)}")
+        "ranged-query; #{query}"
+      else
+        query = query(offset)
+        query += query_clause
+        query += " AND #{query_delta.strip}" if delta
+        "ranged-query; #{query}; #{range_query}"
+      end
+    else
+      if @query.present?
         query = @query.gsub(/\{\{([a-zA-Z0-9_.]+)\}\}/, "\\1 #{ThinkingSphinx.unique_id_expression(adapter, offset)}")
         "query; #{query}"
-    else
-      source_value_without_custom_query(offset, delta)
+      else
+        query = query(offset)
+        query += "WHERE #{query_delta.strip}" if delta
+        "query; #{query}"
+      end
     end
   end
 
--- lib/sphinx_integration/extensions/property.rb
+++ lib/sphinx_integration/extensions/property.rb
@@ -1,11 +1,17 @@
 # coding: utf-8
 module SphinxIntegration::Extensions::Property
+  extend ActiveSupport::Concern
 
-  def available?
+  included do
+    alias_method_chain :available?, :allways_true
+    alias_method_chain :column_available?, :allways_true
+  end
+
+  def available_with_allways_true?
     true
   end
 
-  def column_available?(column)
+  def column_available_with_allways_true?(column)
     true
   end
 
