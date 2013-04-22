41276f9138764f5f3d18e901b6850662a1e9dd17
Merkushin <merkushin.m.s@gmail.com>
2013-04-22 15:12:29 +0600

    thread safe in TS:Index#attributes_types_map

--- lib/sphinx/integration/extensions/thinking_sphinx/index.rb
+++ lib/sphinx/integration/extensions/thinking_sphinx/index.rb
@@ -6,9 +6,15 @@ module Sphinx::Integration::Extensions::ThinkingSphinx::Index
 
   included do
     attr_accessor :merged_with_core, :is_core_index, :mva_sources
+    alias_method_chain :initialize, :mutex
     alias_method_chain :to_riddle, :merged
     alias_method_chain :to_riddle_for_distributed, :merged
     alias_method_chain :all_names, :rt
+  end
+
+  def initialize_with_mutex(model, &block)
+    @mutex = Mutex.new
+    initialize_without_mutex(model, &block)
   end
 
   def to_riddle_with_merged(offset)
@@ -99,8 +105,10 @@ module Sphinx::Integration::Extensions::ThinkingSphinx::Index
   #
   # Returns Hash
   def attributes_types_map
-    return @attributes_type_map if defined?(@attributes_type_map)
-    @attributes_type_map = attributes.inject({}){ |h, attr| h[attr.unique_name.to_s] = attr.type; h }
+    @mutex.synchronize do
+      return @attributes_types_map if @attributes_types_map
+      @attributes_types_map = attributes.inject({}){ |h, attr| h[attr.unique_name.to_s] = attr.type; h }
+    end
   end
 
   def single_query_sql
