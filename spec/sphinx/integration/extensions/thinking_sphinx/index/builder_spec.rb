c425436eac645d5190f2c2cd7de7b775fbddfb0e
Artem Napolskih <napolskih@gmail.com>
2014-09-16 15:03:25 +0600

    feature(index/builder): add delete_attributes and delete_joins features

--- spec/sphinx/integration/extensions/thinking_sphinx/index/builder_spec.rb
+++ spec/sphinx/integration/extensions/thinking_sphinx/index/builder_spec.rb
@@ -35,6 +35,35 @@ describe ThinkingSphinx::Index::Builder do
     its([:as]) { should eq :rubs }
   end
 
+  describe 'delete_joins' do
+    let(:index) do
+      ThinkingSphinx::Index::Builder.generate(ModelWithDisk) do
+        left_join(:rubrics).on('rubrics.id = model.rubric_id').as(:rubs)
+        left_join(:foo).on('rubrics.id = model.rubric_id').as(:rubs)
+        left_join(:bar).on('rubrics.id = model.rubric_id').as(:rubs)
+
+        delete_joins(:rubrics, :bar)
+      end
+    end
+
+    it { index.local_options[:source_joins].should have(1).item }
+    it { index.local_options[:source_joins][:foo].should be_present }
+  end
+
+  describe 'delete_attributes' do
+    let(:index) do
+      ThinkingSphinx::Index::Builder.generate(ModelWithDisk) do
+        has 'foo', :type => :integer, :as => 'foo'
+        has 'bar', :type => :integer, :as => 'bar'
+
+        delete_attributes(:region_id, :bar)
+      end
+    end
+
+    it { index.attributes.should have(4).item } # 3 internal + 1 user
+    it { index.attributes.select { |attr| attr.alias == :foo}.should be_present }
+  end
+
   describe 'limit' do
     let(:index) do
       ThinkingSphinx::Index::Builder.generate(ModelWithDisk) do
